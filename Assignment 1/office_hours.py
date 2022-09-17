912b76131cc6cd01d3b09de2869da33d0bd91f53
Audrey Yang <audrey.butter0930@gmail.com>
2022-09-17 19:13:19 -0400

    Created programs for parts 4 and 5

---
+++ Assignment 1/office_hours.py
@@ -0,0 +1,6 @@
+'''    
+    Extract and print the office hours of the instructor
+    from the CS325 homepage
+    Author: Audrey Yang
+    Date: September 17, 2022
+''' 
---
+++ Assignment 1/weather_status.py
@@ -0,0 +1,6 @@
+'''    
+    Extracts and prints the current weather conditions in Philadelphia
+    from the NOAA's weather server
+    Author: Audrey Yang
+    Date: September 15, 2022
+''' 
