b14dfbfa69d86069b3f829daa6245f9f75aa3cc0
Audrey Yang <audrey.butter0930@gmail.com>
2022-09-18 17:00:33 -0400

    Fine-tuned the regex

--- Assignment 1/office_hours.py
+++ Assignment 1/office_hours.py
@@ -4,3 +4,22 @@
     Author: Audrey Yang
     Date: September 17, 2022
 ''' 
+
+import re
+import urllib.request
+
+def main():
+    url = "https://cs.brynmawr.edu/Courses/cs325/fall2022/"
+    ps = urllib.request.urlopen(url).read().decode('utf-8')
+
+
+    # split intto lines and etc.
+    ps_list = ps.split('\n')
+    text = " ".join(ps_list)
+
+    # get the specificed line
+    my_regex = re.escape("<strong>Office") + r"[\w+ ]+:" + re.escape("</strong>") + r"[\s|\w|:|,|.]+"
+    strong_list = re.findall(my_regex, text)
+    print(strong_list)
+
+main()
