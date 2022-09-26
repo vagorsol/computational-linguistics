0120dd33505ac95597d10d2a0b924db004f0d8c3
Audrey Yang <audrey.butter0930@gmail.com>
2022-09-26 02:50:46 -0400

    word splitting completed
    TODO: why is the sentences not be in ???  lists inside lists???

--- Assignment 2/processing.py
+++ Assignment 2/processing.py
@@ -8,22 +8,44 @@ import re
 from nltk.corpus import gutenberg
 
 def sentence_split(text):
-    # TODO: get punctuation to stay and deal with Mr./Mrs.
+    # split sentences
     split_text = re.split(r'(?<=[\.!?\"])\s\s+', text)
-    return split_text
 
-    # then, parse the splitted sentences into arr in array and then split by words??
+    # then, split each sentences
+    ret = [[] for x in range(len(split_text))] 
+    i = 0
 
-def main():
-    text = gutenberg.raw("austen-emma.txt")
-    ret = sentence_split(text)
-    print(ret[:25], "\n")
+    for sent in split_text:
+        ret[i] = word_split(sent)
+        i += 1
 
-    compare_sent = gutenberg.sents("austen-emma.txt")
-    print(compare_sent[:10], "\n")
-
-    compare_words = gutenberg.words("austen-emma.txt")
-    print(compare_words[:25], "\n")
+    return ret
     
 
-main()
+def word_split(text):
+    #split string by words
+    print(text)
+    split_text = re.split(r"([^\w])", text)
+
+    # parse out spaces
+    ret = [word for word in split_text if word != " " and word != "" and word != "\n" and word != "\t"]
+    print(ret)
+    return ret
+
+def test():
+    print("Own sentence splitter test:")
+    text = gutenberg.raw("austen-emma.txt")
+    ret = sentence_split(text)
+    print(ret[:10])
+
+    # test_txt = "This is a test string, with lots of: punctuations; in it?!."
+    # word_split(test_txt)
+
+    compare_sent = gutenberg.sents("austen-emma.txt")
+    print(compare_sent[:10]) 
+
+    # compare_words = gutenberg.words("austen-emma.txt")
+    # print(compare_words[:50])
+    
+
+test()
