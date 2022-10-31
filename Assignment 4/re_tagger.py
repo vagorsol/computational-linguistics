1a72284eca5d9823131542e2bcf54ea23ef58306
Audrey Yang <audrey.butter0930@gmail.com>
2022-10-31 15:02:26 -0400

    Added rules tailored for the test texts
    For the RE tagger

--- Assignment 4/re_tagger.py
+++ Assignment 4/re_tagger.py
@@ -14,13 +14,17 @@ patterns = [
    (r'^-?[0-9]+(.[0-9]+)?$', 'NUM'), # cardinal numbers
    (r'(The|the|A|a|An|an)$', 'DET'), # articles
    (r'(At|at|On|on|Out|out|Over|over|Per|per|That|that|Up|up|Down|down)$', 'PRT'), # articles
+   (r'I|you|You|he|He|she|She|it|It|we|We|they|They|who|Who|what|What', 'PRON'), # personal pronouns
+   (r'For|for|and|And|nor|Nor|but|But|or|Or|yet|Yet|so|So', 'CONJ'), # conjunctions 
+   (r'is|Is|are|Are|was|Was|were|Were|am|Am|been|Been','VERB'), # verb present singular first person
+   (r'of|Of|in|In|for|For|to|To|on|On|at|At|that|That|with|With|by|By|As|as|from|From'), # adpositions
    (r'.*able$', 'ADJ'), # adjectives
    (r'.*ness$', 'NOUN'), # nouns from adjectives
    (r'.*ly$', 'ADV'), # adverbs
    (r'.*s$', 'NOUN'), # plural noun
-   (r'.*ing$', 'VERB'), # gerund
-   (r'.*ed$', 'VERB'), # simple past
-   (r'.*es$', 'VERB'), # 3rd person singular present
+   (r'.*ing$', 'VERB'), # verb gerund
+   (r'.*ed$', 'VERB'), # verb simple past
+   (r'.*es$', 'VERB'), # verb 3rd person singular present
    (r'.*ould$', 'VERB'), # modal
    (r'.*\'s', 'NOUN'), # possessive noun
    (r'.*', 'NOUN') # noun default
@@ -30,16 +34,14 @@ reTagr = nltk.RegexpTagger(patterns)
 
 def main():
     # open and tokenize the sample texts (into sentences)
-    cs_text = open("CS.txt", "r").read()
-    # print(cs_text)
-    cs_words = nltk.word_tokenize(cs_text)
-    print(cs_words)
+    # cs_text = open("CS.txt", "r").read()
+    # cs_words = nltk.word_tokenize(cs_text)
+    # print(cs_words)
 
-    print(reTagr.tag(cs_words))
+    # print(reTagr.tag(cs_words))
 
-    # animals_text = open("Animals.txt", "r").readlines()
-    # print(animals_text)
-    # animals_words = nltk.word_tokenize(animals_text)
+    animals_text = open("Animals.txt", "r").read()
+    animals_words = nltk.word_tokenize(animals_text)
     # print(animals_words)
-
+    print(reTagr.tag(animals_words))
 main()
--- Assignment 4/stochastic_tagger.py
+++ Assignment 4/stochastic_tagger.py
@@ -14,13 +14,17 @@ patterns = [
    (r'^-?[0-9]+(.[0-9]+)?$', 'NUM'), # cardinal numbers
    (r'(The|the|A|a|An|an)$', 'DET'), # articles
    (r'(At|at|On|on|Out|out|Over|over|Per|per|That|that|Up|up|Down|down)$', 'PRT'), # articles
+   (r'I|you|You|he|He|she|She|it|It|we|We|they|They|who|Who|what|What', 'PRON'), # personal pronouns
+   (r'For|for|and|And|nor|Nor|but|But|or|Or|yet|Yet|so|So', 'CONJ'), # conjunctions 
+   (r'is|Is|are|Are|was|Was|were|Were|am|Am|been|Been','VERB'), # verb present singular first person
+   (r'of|Of|in|In|for|For|to|To|on|On|at|At|that|That|with|With|by|By|As|as|from|From'), # adpositions
    (r'.*able$', 'ADJ'), # adjectives
    (r'.*ness$', 'NOUN'), # nouns from adjectives
    (r'.*ly$', 'ADV'), # adverbs
    (r'.*s$', 'NOUN'), # plural noun
-   (r'.*ing$', 'VERB'), # gerund
-   (r'.*ed$', 'VERB'), # simple past
-   (r'.*es$', 'VERB'), # 3rd person singular present
+   (r'.*ing$', 'VERB'), # verb gerund
+   (r'.*ed$', 'VERB'), # verb simple past
+   (r'.*es$', 'VERB'), # verb 3rd person singular present
    (r'.*ould$', 'VERB'), # modal
    (r'.*\'s', 'NOUN'), # possessive noun
    (r'.*', 'NOUN') # noun default
@@ -35,6 +39,7 @@ bs = brown.sents(categories="news")
 n = int(len(bs) * 0.9)
 trainset = bts[:n]
 testset = bts[n:]
+print(len(testset))
 
 # stochastic tagger
 dTagr = nltk.DefaultTagger("NOUN")
@@ -43,4 +48,11 @@ biTagr = nltk.BigramTagger(trainset, backoff = uTagr)
 
 print(biTagr.accuracy(testset))
 
+cs_text = open("CS.txt", "r").read()
+cs_words = nltk.word_tokenize(cs_text)
+print(biTagr.tag(cs_words))
+
+animals_text = open("Animals.txt", "r").read()
+animals_words = nltk.word_tokenize(animals_text)
+print(biTagr.tag(animals_words))
 # TODO: RE backoff tagger and RE itself evaluation
