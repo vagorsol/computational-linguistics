838a873dd6ac4139a2401bebff26e7c66b2a0fd0
Audrey Yang <audrey.butter0930@gmail.com>
2022-10-31 16:39:32 -0400

    An attempt in the wrong direction was made

--- Assignment 4/stochastic_tagger.py
+++ Assignment 4/stochastic_tagger.py
@@ -6,9 +6,9 @@
 
 import nltk 
 from nltk.corpus import brown
+from nltk.tag import hmm
 
 # RE tagger
-# universal tagset not included: ADP, CONJ, PRT, X (other), PRON
 patterns = [
    (r'[\.,:;?!]', '.'), # punctuation
    (r'^-?[0-9]+(.[0-9]+)?$', 'NUM'), # cardinal numbers
@@ -35,24 +35,24 @@ reTagr = nltk.RegexpTagger(patterns)
 # NEWS CORPUS
 nts = brown.tagged_sents(categories="news", tagset="universal")
 
-# divide the text into train and test sets
-n = int(len(nts) * 0.9)
-trainset_news = nts[:n]
-testset_news = nts[n:]
+# # divide the text into train and test sets
+# n = int(len(nts) * 0.9)
+# trainset_news = nts[:n]
+# testset_news = nts[n:]
 
-# stochastic tagger
-dTagr = nltk.DefaultTagger("NOUN")
-uTagr = nltk.UnigramTagger(trainset_news, backoff = dTagr)
-biTagr = nltk.BigramTagger(trainset_news, backoff = uTagr)
+# # stochastic tagger
+# dTagr = nltk.DefaultTagger("NOUN")
+# uTagr = nltk.UnigramTagger(trainset_news, backoff = dTagr)
+# biTagr = nltk.BigramTagger(trainset_news, backoff = uTagr)
 
-# RE backoff tagger
-uREtagr = nltk.UnigramTagger(trainset_news, backoff = reTagr)
-biREtagr = nltk.BigramTagger(trainset_news, backoff = uREtagr)
+# # RE backoff tagger
+# uREtagr = nltk.UnigramTagger(trainset_news, backoff = reTagr)
+# biREtagr = nltk.BigramTagger(trainset_news, backoff = uREtagr)
 
-print("News Brown Corpus")
-print(f"Bigram Default Tagger Backoff Accuracy: {biTagr.accuracy(testset_news)}")
-print(f"Bigram RE Backoff Accuracy: {biREtagr.accuracy(testset_news)}")
-print(f"RE Tagger Accuracy: {reTagr.accuracy(testset_news)}")
+# print("News Brown Corpus")
+# print(f"Bigram Default Tagger Backoff Accuracy: {biTagr.accuracy(testset_news)}")
+# print(f"Bigram RE Backoff Accuracy: {biREtagr.accuracy(testset_news)}")
+# print(f"RE Tagger Accuracy: {reTagr.accuracy(testset_news)}")
 
 # FICTION CORPUS
 fts = brown.tagged_sents(categories="fiction", tagset="universal")
@@ -61,22 +61,52 @@ trainset_fic = nts[:n]
 testset_fic = nts[n:]
 
 # stochastic tagger
-dTagr = nltk.DefaultTagger("NOUN")
-uTagr = nltk.UnigramTagger(trainset_fic, backoff = dTagr)
-biTagr = nltk.BigramTagger(trainset_fic, backoff = uTagr)
+# dTagr = nltk.DefaultTagger("NOUN")
+# uTagr = nltk.UnigramTagger(trainset_fic, backoff = dTagr)
+# biTagr = nltk.BigramTagger(trainset_fic, backoff = uTagr)
 
 # RE backoff tagger
 uREtagr = nltk.UnigramTagger(trainset_fic, backoff = reTagr)
 biREtagr = nltk.BigramTagger(trainset_fic, backoff = uREtagr)
 
-print("\nFiction Brown Corpus")
-print(f"Bigram Default Tagger Backoff Accuracy: {biTagr.accuracy(testset_fic)}")
-print(f"Bigram RE Backoff Accuracy: {biREtagr.accuracy(testset_fic)}")
-print(f"RE Tagger Accuracy: {reTagr.accuracy(testset_fic)}")
+# Hidden Markov Model
+# hmmTagr = hmm.HiddenMarkovModelTagger.train(trainset_fic)
+
+# print("\nFiction Brown Corpus")
+# print(f"Bigram Default Tagger Backoff Accuracy: {biTagr.accuracy(testset_fic)}")
+# print(f"Bigram RE Backoff Accuracy: {biREtagr.accuracy(testset_fic)}")
+# print(f"RE Tagger Accuracy: {reTagr.accuracy(testset_fic)}")
 
 # open and tokenize the sample texts
 cs_text = open("CS.txt", "r").read()
+cs_sents = nltk.sent_tokenize(cs_text)
 cs_words = nltk.word_tokenize(cs_text)
+
+# tagged = biREtagr.tag(cs_words)
+# print(tagged)
+
+cs_tagged = []
+for i in range(len(cs_sents)):
+    linetok = nltk.word_tokenize(cs_sents[i])
+    cs_tagged.append(biREtagr.tag(linetok))
+
+
+print("\nOutput for the CS Text")
+# # print(f"Bigram Default Tagger Backoff Accuracy: {biTagr.accuracy(cs_words)}")
+# print(testset_fic[:5])
+
+
+print(f"Bigram RE Backoff Accuracy: {biREtagr.accuracy(cs_tagged)}")
+# print(f"RE Tagger Accuracy: {reTagr.accuracy(cs_words)}")
+# print(f"Hidden Markov Model Accuracy:{hmmTagr.accuracy(cs_words)}")
+# print(f"NTLK's Tagger: {nltk.pos_tag(cs_words)}")
 
 animals_text = open("Animals.txt", "r").read()
 animals_words = nltk.word_tokenize(animals_text)
+
+# print("\nAnimals Text Testing")
+# print(f"Bigram Default Tagger Backoff Accuracy: {biTagr.accuracy(animals_words)}")
+# print(f"Bigram RE Backoff Accuracy: {biREtagr.accuracy(animals_words)}")
+# print(f"RE Tagger Accuracy: {reTagr.accuracy(animals_words)}")
+# print(f"Hidden Markov Model Accuracy:{hmmTagr.accuracy(animals_words)}")
+# print(f"NTLK's Tagger: {nltk.pos_tag(animals_words)}")
